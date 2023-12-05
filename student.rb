9190d4a30d8e0a84fe42f07cb3912899b3e455dd
user-faiz <email2faizm@gmail.com>
2023-12-05 21:20:29 +0530

    Add Method to return the string

--- student.rb
+++ student.rb
@@ -1,5 +1,12 @@
 require_relative 'Person'
 
 class Student < Person
-  
+  def initialize(id, name: "Unknown", age: 0, parent_permission: true, classroom:)
+    super(id, name: name, age: age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\\(ツ)/¯"
+  end
 end
