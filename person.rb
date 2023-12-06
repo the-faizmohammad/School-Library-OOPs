9f49908d2e87901f821d56d86142500af7d1baac
Faiz Mohammad <email2faizm@gmail.com>
2023-12-06 19:27:43 +0530

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,20 +1,21 @@
-def initialize(id, name: 'Unknown', age: 0, parent_permission: true)
-  @id = id
-  @name = name
-  @age = age
-  @parent_permission = parent_permission
-end
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
 
-attr_writer :name
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-attr_writer :age
-
-def can_use_services?
-  of_age? || @parent_permission
-end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
   private
 
-def of_age?
-  @age >= 18
+  def of_age?
+    @age >= 18
+  end
 end
