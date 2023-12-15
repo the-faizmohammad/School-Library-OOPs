e43f8435221a603a0c781b1359ef8287627fedb9
Obimbo07 <obimboausts@gmail.com>
2023-12-15 22:04:37 +0300

    test-classes

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,8 @@
+require '/classroom'
+
+RSpec.describe Classroom do
+    it 'creates a classroom with a label' do
+      classroom = Classroom.new('Room 101')
+      expect(classroom.label).to eq('Room 101')
+    end
+  end
